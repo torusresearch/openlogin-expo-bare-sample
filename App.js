90b322755887e5c45928723f924b7ef346a64bb1
michaellee8 <ckmichael8@gmail.com>
2022-02-17 20:48:16 +0800

    feat: apply example app from sdk

--- App.js
+++ App.js
@@ -1,111 +1,57 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow strict-local
- */
+import { StatusBar } from "expo-status-bar";
+import { useState } from "react";
+import { StyleSheet, Text, View, Button } from "react-native";
+import OpenLogin, { LoginProvider, Network } from "openlogin-expo-sdk";
+import Constants, { AppOwnership } from "expo-constants";
+import * as Linking from "expo-linking";
+import { URL } from "react-native-url-polyfill";
 
-import React from 'react';
-import {
-  SafeAreaView,
-  ScrollView,
-  StatusBar,
-  StyleSheet,
-  Text,
-  useColorScheme,
-  View,
-} from 'react-native';
+const scheme = "openloginexposdkexampleexpo";
 
-import {
-  Colors,
-  DebugInstructions,
-  Header,
-  LearnMoreLinks,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
+const resolvedRedirectUrl =
+  Constants.appOwnership == AppOwnership.Expo ||
+  Constants.appOwnership == AppOwnership.Guest
+    ? new URL(Linking.createURL("openlogin", {}))
+    : new URL(Linking.createURL("openlogin", { scheme: scheme }));
 
-const Section = ({children, title}) => {
-  const isDarkMode = useColorScheme() === 'dark';
+export default function App() {
+  const [key, setKey] = useState("");
+  const [errorMsg, setErrorMsg] = useState("");
+  const login = async () => {
+    try {
+      const openlogin = new OpenLogin({
+        clientId:
+          "BC5bANkU4-fil7C5s1uKzRfF0VGqbuaxDQiLnQ8WgF7SEA32lGegAhu7dk4dZf3Rk397blIvfWytXwsRvs9dOaQ",
+        network: Network.TESTNET,
+      });
+      const state = await openlogin.login({
+        loginProvider: LoginProvider.GOOGLE,
+        redirectUrl: resolvedRedirectUrl,
+      });
+      setKey(state.privKey || "no key");
+    } catch (e) {
+      console.error(e);
+      setErrorMsg(String(e));
+    }
+  };
   return (
-    <View style={styles.sectionContainer}>
-      <Text
-        style={[
-          styles.sectionTitle,
-          {
-            color: isDarkMode ? Colors.white : Colors.black,
-          },
-        ]}>
-        {title}
-      </Text>
-      <Text
-        style={[
-          styles.sectionDescription,
-          {
-            color: isDarkMode ? Colors.light : Colors.dark,
-          },
-        ]}>
-        {children}
-      </Text>
+    <View style={styles.container}>
+      <Text>Key: {key}</Text>
+      <Text>Error: {errorMsg}</Text>
+      <Text>Linking URL: {resolvedRedirectUrl.href}</Text>
+      <Text>appOwnership: {Constants.appOwnership}</Text>
+      <Text>executionEnvironment: {Constants.executionEnvironment}</Text>
+      <Button title="Login with OpenLogin" onPress={login} />
+      <StatusBar style="auto" />
     </View>
   );
-};
-
-const App = () => {
-  const isDarkMode = useColorScheme() === 'dark';
-
-  const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
-  };
-
-  return (
-    <SafeAreaView style={backgroundStyle}>
-      <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
-      <ScrollView
-        contentInsetAdjustmentBehavior="automatic"
-        style={backgroundStyle}>
-        <Header />
-        <View
-          style={{
-            backgroundColor: isDarkMode ? Colors.black : Colors.white,
-          }}>
-          <Section title="Step One">
-            Edit <Text style={styles.highlight}>App.js</Text> to change this
-            screen and then come back to see your edits.
-          </Section>
-          <Section title="See Your Changes">
-            <ReloadInstructions />
-          </Section>
-          <Section title="Debug">
-            <DebugInstructions />
-          </Section>
-          <Section title="Learn More">
-            Read the docs to discover what to do next:
-          </Section>
-          <LearnMoreLinks />
-        </View>
-      </ScrollView>
-    </SafeAreaView>
-  );
-};
+}
 
 const styles = StyleSheet.create({
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-  },
-  highlight: {
-    fontWeight: '700',
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
-
-export default App;
